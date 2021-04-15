7c38b442e8a2df24f70d54dbc0dbfe33cf88bc95
Kevin Ayala <ayala.kevin99@gmail.com>
2021-04-15 09:17:19 -0700

    added most of code for ner, trained some data

--- ner/ner_model.py
+++ ner/ner_model.py
@@ -2,8 +2,88 @@ import numpy as np
 import pandas as pd
 import spacy
 import re
+import trained_data
+import random
+import plac
+from pathlib import Path
+from tqdm import tqdm  # loading bar
+
+model = None
+output_dir = "./output"
+n_iter = 100
 
 nlp = spacy.load("en_core_web_sm")
 
-#import data
-df = pd.read_csv('../scraper/output.csv')
+if model is not None:
+    nlp1 = spacy.load(model)  # load existing spaCy model
+    print("Loaded model '%s'" % model)
+else:
+    nlp1 = spacy.blank('en')  # create blank Language class
+    print("Created blank 'en' model")
+
+# create the built-in pipeline components and add them to the pipeline
+    # nlp.create_pipe works for built-ins that are registered with spaCy
+if 'ner' not in nlp1.pipe_names:
+    ner = nlp1.create_pipe('ner')
+    nlp1.add_pipe(ner, last=True)
+# otherwise, get it so we can add labels
+else:
+    ner = nlp1.get_pipe('ner')
+
+# add labels
+for _, annotations in trained_data.TRAIN_DATA:
+    for ent in annotations.get('entities'):
+        ner.add_label(ent[2])
+
+    # get names of other pipes to disable them during training
+other_pipes = [pipe for pipe in nlp1.pipe_names if pipe != 'ner']
+with nlp1.disable_pipes(*other_pipes):  # only train NER
+    optimizer = nlp1.begin_training()
+    for itn in range(n_iter):
+        random.shuffle(trained_data.TRAIN_DATA)
+        losses = {}
+        for text, annotations in tqdm(trained_data.TRAIN_DATA):
+            nlp1.update(
+                [text],  # batch of texts
+                [annotations],  # batch of annotations
+                drop=0.5,  # dropout - make it harder to memorise data
+                sgd=optimizer,  # callable to update weights
+                losses=losses)
+        print(losses)
+
+
+# test the trained model
+for text, _ in trained_data.TRAIN_DATA:
+    doc = nlp1(x)
+    print('Entities', [(ent.text, ent.label_) for ent in doc.ents])
+
+
+# save model to output directory
+if output_dir is not None:
+    output_dir = Path(output_dir)
+if not output_dir.exists():
+    output_dir.mkdir()
+nlp.to_disk(output_dir)
+print("Saved model to", output_dir)
+
+
+# test the saved model
+print("Loading from", output_dir)
+nlp2 = spacy.load(output_dir)
+for text, _ in trained_data.TRAIN_DATA:
+    doc = nlp2(text)
+    print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
+
+# nlp = spacy.load("en_core_web_sm")
+
+# # import data
+# df = pd.read_csv('../data_processing/processed_output_csvs/final_output.csv')
+
+# for index, row in df.iterrows():
+#     # print(list(df.columns))
+#     print(row['Finished Text'])
+
+#     doc = nlp(row['Finished Text'])
+
+#     ents = [(e.text, e.start_char, e.end_char, e.label_) for e in doc.ents]
+#     print(ents)
---
+++ ner/trained_data.py
@@ -0,0 +1,80 @@
+TRAIN_DATA = [
+    (0, {
+        'entities': [(48, 58, 'ATTRIBUTE'), (751, 758, 'SKILL'), (759, 765, 'SKILL'), (768, 774, 'SKILL'),
+                     (777, 788, 'SKILL'), (791, 794, 'SKILL'), (797, 804,
+                                                                'SKILL'), (805, 811, 'SKILL'), (854, 895, 'ATTRIBUTE'),
+                     (919, 924, 'SKILL')]
+    }),
+    (1, {
+        'entities': [
+            (1343, 1354, 'SKILL'), (1402, 1406, 'SKILL'), (1407,
+                                                           1412, 'SKILL'), (1443, 1450, 'SKILL'),
+            (1453, 1462, 'SKILL'), (1465, 1477, 'SKILL'), (1480, 1489, 'SKILL')
+        ]
+    }),
+    (2, {
+        'entities': [
+            (918, 321, 'SKILL'), (321, 331, 'SKILL'), (334, 358,
+                                                       'SKILL'), (341, 344, 'SKILL'), (347, 357, 'SKILL'),
+            (360, 366, 'SKILL'), (369, 377, 'SKILL'), (375, 379,
+                                                       'SKILL'), (394, 400, 'SKILL'), (403, 409, 'SKILL'),
+            (412, 422, 'SKILL'), (425, 430, 'SKILL'), (431, 442, 'SKILL')
+        ]
+    }),
+    (3, {
+        'entities': [
+            (336, 347, 'ATTRIBUTE'), (516, 531, 'SKILL'), (534, 549,
+                                                           'SKILL'), (552, 568, 'SKILL'), (571, 585, 'SKILL'),
+            (588, 607, 'SKILL'), (622, 652, 'SKILL')
+        ]
+    }),
+    (4, {
+        'entities': [
+            (79, 83, 'SKILL'), (86, 89, 'SKILL'), (92, 94, 'SKILL'), (97,
+                                                                      100, 'SKILL'), (103, 109, 'SKILL'), (112, 116, 'SKILL'),
+            (119, 122, 'SKILL'), (125, 135, 'SKILL'), (198, 211, 'SKILL'), (214,
+                                                                            229, 'SKILL'), (230, 245, 'SKILL'), (269, 289, 'SKILL')
+        ]
+    }),
+    (5, {
+        'entities': [
+            (188, 196, 'ATTRIBUTE'), (197, 211, 'ATTRIBUTE'), (188, 211,
+                                                               'ATTRIBUTE'), (217, 228, 'ATTRIBUTE'), (350, 358, 'SKILL'),
+            (472, 491, 'ATTRIBUTE'), (501, 516,
+                                      'SKILL'), (600, 607, 'SKILL'), (643, 654, 'SKILL')
+        ]
+    }),
+    (6, {
+        'entities': [
+            (37, 41, 'SKILL'), (84, 92, 'ATTRIBUTE'), (187, 196, 'SKILL'), (199,
+                                                                            202, 'SKILL'), (205, 210, 'SKILL'), (358, 373, 'SKILL'),
+            (374, 376, 'SKILL')
+        ]
+    }),
+    (7, {
+        'entities': [
+            (47, 56, 'SKILL'), (57, 70, 'SKILL'), (25, 34, 'ATTRIBUTE'), (35,
+                                                                          46, 'ATTRIBUTE'), (47, 70, 'SKILL'), (327, 332, 'SKILL'),
+            (335, 339, 'SKILL'), (342, 352, 'SKILL'), (355, 361, 'SKILL'), (383,
+                                                                            392, 'SKILL'), (393, 404, 'SKILL'), (443, 452, 'SKILL')
+        ]
+    }),
+    (8, {'entities': [
+        (210, 224, 'ATTRIBUTE'), (225, 234, 'ATTRIBUTE'), (299, 308, 'ATTRIBUTE')
+    ]
+    }),
+    (9, {'entities': [
+        (133, 148, 'SKILL'), (174, 184, 'SKILL'), (220, 231,
+                                                   'ATTRIBUTE'), (234, 256, 'ATTRIBUTE'), (257, 267, 'ATTRIBUTE'),
+        (268, 272, 'SKILL'), (283, 311, 'SKILL')
+    ]
+    }),
+    (10, {'entities': [
+        (37, 47, 'ATTRIBUTE'), (48, 59, 'ATTRIBUTE'), (129, 140,
+                                                       'ATTRIBUTE'), (158, 176, 'SKILL'), (205, 208, 'SKILL'),
+        (233, 238, 'SKILL'), (371, 393, 'SKILL'), (371, 399,
+                                                   'SKILL'), (433, 452, 'SKILL'), (475, 492, 'SKILL')
+    ]
+    })
+
+]
