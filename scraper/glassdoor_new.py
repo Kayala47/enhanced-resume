828ca4462cb0f134862422e9f19b2d4deef1cad9
aidan <aid1@bamba.us>
2022-05-06 13:06:20 -0700

    added single scrape

--- scraper/glassdoor_new.py
+++ scraper/glassdoor_new.py
@@ -18,7 +18,52 @@ from bs4 import BeautifulSoup
 
 THREADS = 30 # MUST BE FACTOR OF LISTINGS PER PAGE (30)
 
-def scrape_single(listings, scraped):
+def scrape_single(query):
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+    kw_param = "%20".join(query.split(" "))
+    url = f"https://www.glassdoor.com/Job/jobs.htm?sc.keyword={kw_param}"
+
+    driver.get(url)  # open jobs page
+    
+    listing = (
+        WebDriverWait(driver, 10)
+        .until(
+            EC.presence_of_element_located(
+                (
+                    By.CLASS_NAME,
+                    "react-job-listing",
+                )
+            )
+        )
+    )
+
+    a_tag = listing.find_elements_by_tag_name('a')[1]
+    url = a_tag.get_attribute('href')
+
+    name = a_tag.text
+    title = listing.get_attribute('data-normalize-job-title')
+    loc = listing.get_attribute('data-job-loc')
+
+    r = requests.get(url, headers={
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36'
+    })
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    desc = soup.find('div', {'id': 'JobDescriptionContainer'}).text
+
+    return desc
+    # return {
+    #     "Company Name": name,
+    #     "Job Title": title,
+    #     "Job Location": loc,
+    #     "Job Description": desc,
+    # }
+
+def thread_scrape(listings, scraped):
     for listing in listings:
         a_tag = listing.find_elements_by_tag_name('a')[1]
         url = a_tag.get_attribute('href')
@@ -47,7 +92,7 @@ def scrape_page(job_listings):
     tsz = int(len(job_listings)/THREADS)
 
     for n in range(0,THREADS):
-        t = Thread(target=scrape_single, args=(job_listings[n*tsz:(n+1)*tsz], scraped_listings))
+        t = Thread(target=thread_scrape, args=(job_listings[n*tsz:(n+1)*tsz], scraped_listings))
         threads.append(t)
         t.start()
 
