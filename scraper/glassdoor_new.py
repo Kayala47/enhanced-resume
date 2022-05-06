7dad8c810354d5f9c7d9ea94f4198a4abf83a40d
aidan <aid1@bamba.us>
2022-05-06 11:17:55 -0700

    threaded scraper

--- scraper/glassdoor_new.py
+++ scraper/glassdoor_new.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.common.action_chains import ActionChains
+
 from webdriver_manager.chrome import ChromeDriverManager
 
-import json
+import time
+import sys
 import pandas as pd
 
 from threading import Thread
@@ -34,20 +41,7 @@ def scrape_single(listings, scraped):
             "Job Description": desc,
         })
 
-def scrape_page(query):
-    options = webdriver.ChromeOptions()
-    # options.add_argument("headless")
-    # options.add_argument("start-maximized")
-
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-
-    kw_param = "%20".join(query.split(" "))
-    url = f"https://www.glassdoor.com/Job/jobs.htm?sc.keyword={kw_param}"
-
-    driver.get(url)  # open jobs page
-
-    job_listings = driver.find_elements_by_class_name("react-job-listing")
-
+def scrape_page(job_listings):
     scraped_listings = []
     threads = []
     tsz = int(len(job_listings)/THREADS)
@@ -62,7 +56,64 @@ def scrape_page(query):
         
     return scraped_listings
 
+def scrape(query, num_pages):
+    options = webdriver.ChromeOptions()
+    # options.add_argument("headless")
+    # options.add_argument("start-maximized")
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+    kw_param = "%20".join(query.split(" "))
+    url = f"https://www.glassdoor.com/Job/jobs.htm?sc.keyword={kw_param}"
+
+    driver.get(url)  # open jobs page
+    scraped = []
+
+    for _ in range(0,num_pages):
+        (
+            WebDriverWait(driver, 10)
+            .until(
+                EC.presence_of_element_located(
+                    (
+                        By.CLASS_NAME,
+                        "react-job-listing",
+                    )
+                )
+            )
+        )
+
+        job_listings = driver.find_elements_by_class_name("react-job-listing")
+
+        scraped += scrape_page(job_listings)
+
+        try:
+            driver.find_element_by_class_name("nextButton").click()
+        except ElementClickInterceptedException:
+            elem = driver.find_element_by_class_name("modal_closeIcon-svg")
+            ac = ActionChains(driver)
+            ac.move_to_element(elem).click().perform()  # move cursor to X SVG and click
+            time.sleep(2)
+            driver.find_element_by_class_name("nextButton").click()
+
+    return pd.DataFrame(scraped)
+
+def write_to_csv(df, path):
+    pd.set_option("display.max_colwidth", None)
+    df.to_csv(path, index=False)
+
 if __name__ == "__main__":
-    listings = scrape_page('machine learning')
-    with open('test.json', 'w') as f:
-        json.dump(listings, f)
+    if len(sys.argv) != 3:
+        print(sys.argv)
+        print(
+            "Incorrect number of arguments, should be 'python glassdoor_scraper.py <search_query> <num_pages>'"
+        )
+    else:
+        try:
+            query = str(sys.argv[1])
+            num_pages = int(sys.argv[2])
+        except:
+            print(
+                "Incorrect argument format, should be 'python glassdoor_scraper.py <search_query> <num_pages>'"
+            )
+        scraped = scrape('machine learning', num_pages)
+        write_to_csv(scraped, './output.csv')
